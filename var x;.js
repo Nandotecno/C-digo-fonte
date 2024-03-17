c71ae80f042da2ed52cb7908f7462ccff03d321f
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2024-03-17 11:18:28 -0300

    Add files via upload

---
+++ var x;.js
@@ -0,0 +1,10 @@
+var x;
+var n1;
+var n2;
+x = prompt(“entre com uma letra para escolher a operação matemática: (s)ubtração, (m)ultiplicação ou (d)ivisão.”);
+n1 = prompt(“entre com o primeiro número”);
+n2 = prompt(“entre com o segundo número”);
+alert(“O resultado é...”);
+if (x == “s”) alert(n1 - n2);
+if (x == “m”) alert(n1 * n2);
+if (x == “d”) alert(n1 / n2);
